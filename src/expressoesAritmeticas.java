3acee48dd137538898560f7f2a33c721fd7f1909
ThalisonSilva <devthalison@gmail.com>
2024-07-30 08:43:07 -0300

    expressoesAritimeticas

---
+++ src/expressoesAritmeticas.java
@@ -0,0 +1,45 @@
+public class expressoesAritmeticas {
+   public static void main(String[] args) {
+    //Adição
+    int a = 10;
+    int b = 25; 
+    int sum = a + b;
+
+    System.out.println("Soma: " + sum); 
+
+    //Subratração
+    int c = 105;
+    int d = 55;
+    int sub = c - d;
+
+    System.out.println("Sob:" + sub);
+
+   // Concatenando strings com (+)
+    String hello = "Olá";
+    String world = "Mundo!";
+    String sauldacao = hello + " " + world;
+    
+    System.out.println(sauldacao);
+
+    
+    //Multiplicação
+    int e = 20;
+    int f = 5;
+    int mult = e * f;
+
+    System.out.println("Mult:" + mult);
+
+    //Divisão
+    int g = 50;
+    int h = 2;
+    int div = g /h;
+    System.out.println("Div: " + div);
+
+    //Módulo
+    int i = 100;
+    int j = 4;
+    int mod = i % j;
+    System.out.println("Mod: " + mod);
+
+    }
+}
