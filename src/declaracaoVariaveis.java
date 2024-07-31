b45cffcfa9c0d4065d0c154c769a8920e119e3a9
ThalisonSilva <devthalison@gmail.com>
2024-07-31 11:33:39 -0300

    decla/variaveis

--- src/declaracaoVariaveis.java
+++ src/declaracaoVariaveis.java
@@ -1,5 +1,15 @@
 public class declaracaoVariaveis {
     public static void main(String[] args) {
         
+        // Declaração de Variáveis: 
+        // <tipo> <nome> = <valor inicial> (o valor e opcional)
+
+        // Exemplo de declaração com a inicializando.
+        int idade = 26;
+        double salario = 1.900;
+        String nome = "Thalison"; 
+
+
+        
     }
 }
